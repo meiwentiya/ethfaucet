603bd945d89bbb4eb54fe6b3c683ae4b65bb7733
zhangpanyi <zhangpanyi@live.com>
2018-03-29 18:51:03 +0800

    update

--- ethereum/transfer.js
+++ ethereum/transfer.js
@@ -133,3 +133,74 @@ waitgroup
 };
 
 module.exports = Transfer;
+
+
+
+// Copyright (c) 2012 Sam Nguyen <samxnguyen@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+(function(){
+    var WaitGroup = function(){
+        this.total = 0; // Number of total items
+        this.ready = 0; // Number of items ready
+    };
+    
+    WaitGroup.prototype.add = function WaitGroupAdd(){
+        this.total++;
+    };
+    
+    WaitGroup.prototype.done = function WaitGroupDone(){
+        this.ready++;
+    };
+    
+    WaitGroup.prototype.wait = function(fn) {
+      var self = this;
+      setTimeout(function(){
+        if(self.ready == self.total) return fn();
+        self.wait(fn);
+      }, 0);
+    };
+    
+    // Export to node.js
+    if(typeof(module) !== "undefined") {
+        module.exports = WaitGroup;
+    }
+    // Export to browser
+    else {
+        window.WaitGroup = WaitGroup;
+    }
+    
+    })();
+    async function test() {
+        console.log('Hello')
+        await sleep(1000)
+        console.log('world!')
+      }
+      
+      function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+      }
+      
+      test()
+      
+      作者：贺师俊
+      链接：https://www.zhihu.com/question/31636244/answer/52835780
+      来源：知乎
+      著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
